726df8e9c35e105e61ac42d4bad094dac321e890
ThomasPineiro <onixyros@gmail.com>
2017-12-05 11:01:01 +0100

    Add files via upload

--- js/style.js
+++ js/style.js
@@ -4,20 +4,10 @@ function masquer_div(id)
        document.getElementById(id).style.display = 'none';
 }
 
-//Cette fonction sert a afficher la date du jour en format jour/mois/année
-
-// function datefr(){
-// var jour= new Array("Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi");
-// var mois = new Array("janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre");
-// var date= new Date();
-// var message = jours[date.getDay()] + " ";   // nom du jour
-// 	message += numjour.getDate() + " ";   // numero du jour
-// 	message += mois[date.getMonth()] + " ";   // mois
-//  	message += date.getFullYear();
-//  	document.getElementById(date).innerHTML = message;
-// }
-
-function myFunction() {
+//Cette fonction sert a afficher la date du jour en format jour/mois/année, merci a Pilou pour le code
+// mais pas l'indentation :d
+function myFunction() 
+{
     var d = new Date();
     var weekday = new Array(7);
 	//weekday[-2] = "Vendredi ";
@@ -45,11 +35,7 @@ function myFunction() {
     mois[11] =  "Décembre";
     var num = new Date();
 	var rest = num.getDate();
-	
-    var n = weekday[d.getDay()];
-    
-    
-    
+	var n = weekday[d.getDay()];
     var m = mois[d.getMonth()];
     document.getElementById("date").innerHTML = n+rest+" "+m;
     //var n2 = weekday[d.getDay()-1];
@@ -59,3 +45,13 @@ function myFunction() {
 }
 myFunction()
 
+// Fonctions de redirection
+function redirection_accueil()
+{
+    document.location.href="index.html";
+}
+
+function redirection_modif()
+{
+    document.location.href="formulaire_modifications.html";
+}
